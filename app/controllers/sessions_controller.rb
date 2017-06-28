c7e231a72ec8db1dac1d0c7cd579e23aa2e659a6
Phuc Hoang <phuc.uet@gmail.com>
2017-06-28 02:15:43 -0400

    basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,16 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      redirect_back_or user
     else
-      flash.now[:danger] = t ".error_login"
+      flash.now[:danger] = t(".error_login")
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
