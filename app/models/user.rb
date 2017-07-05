358e3245c093b9e381290adfcac728a1b619d3cd
Phuc Hoang <phuc.uet@gmail.com>
2017-07-04 22:40:14 -0400

    update, edit users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: Settings.email.maximum},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true,
-    length: {minimum: Settings.password.minimum}
+    length: {minimum: Settings.password.minimum}, allow_nil: true
 
   has_secure_password
 
@@ -36,5 +36,9 @@ class User < ApplicationRecord
 
   def forget
     update_attributes remember_digest: nil
+  end
+
+  def current_user? user
+    self == user
   end
 end
